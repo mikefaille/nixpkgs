c4acbf1c65539aaf208b4290b7743cb4e968effa
Michaël Faille <michael@faille.io>
2016-09-09 09:55:28 -0400

    Upgrade (Kubernetes) : go1.6 --> go1.7

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13726,7 +13726,7 @@ in
   ksuperkey = callPackage ../tools/X11/ksuperkey { };
 
   kubernetes = callPackage ../applications/networking/cluster/kubernetes {
-    go = go_1_6;
+    go = go_1_7;
   };
 
 
