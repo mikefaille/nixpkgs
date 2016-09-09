f030218e1971716c32cd2aa05fcd32472e04750b
Michaël Faille <michael@faille.io>
2016-09-09 09:54:59 -0400

    Add docker-machine as package

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12720,6 +12720,9 @@ in
 
   docker-gc = callPackage ../applications/virtualization/docker/gc.nix { };
 
+  docker-machine-driver-kvm = callPackage ../applications/networking/cluster/docker-machine-driver-kvm { };
+
+
   doodle = callPackage ../applications/search/doodle { };
 
   drumgizmo = callPackage ../applications/audio/drumgizmo { };
